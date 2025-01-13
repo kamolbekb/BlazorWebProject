17e480336c5e4515dd70c7b63d6426adee8ceb8a
kamolbekb <118444680+kamolbekb@users.noreply.github.com>
2025-01-13 19:03:51 +0500

    pagination implemented

--- ArizaApp/Client/Nafaqa.Web/Program.cs
+++ ArizaApp/Client/Nafaqa.Web/Program.cs
@@ -1,5 +1,7 @@
+using CurrieTechnologies.Razor.SweetAlert2;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor;
 using MudBlazor.Services;
 using Nafaqa.Web;
 using Nafaqa.Web.Services;
@@ -7,6 +9,20 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddMudServices();
+builder.Services.AddSweetAlert2();
+builder.Services.AddMudServices(config =>
+{
+    config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomLeft;
+
+    config.SnackbarConfiguration.PreventDuplicates = false;
+    config.SnackbarConfiguration.NewestOnTop = false;
+    config.SnackbarConfiguration.ShowCloseIcon = true;
+    config.SnackbarConfiguration.VisibleStateDuration = 3000;
+    config.SnackbarConfiguration.HideTransitionDuration = 500;
+    config.SnackbarConfiguration.ShowTransitionDuration = 500;
+    config.SnackbarConfiguration.SnackbarVariant = Variant.Filled;
+});
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:5501") });
 builder.Services.AddScoped<PetitionService>();
 await builder.Build().RunAsync();
